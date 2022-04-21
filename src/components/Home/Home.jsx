c056709b8b4c67db9541331472547a190c8fe920
sani2608 <sanikumarsahani@gmail.com>
2022-04-22 02:13:06 +0530

    home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
-import './Home.css';
-const Home = () => {
-  return (
-    <div>Home</div>
-  )
+import React, { Component } from "react";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import "./Home.css";
+import homeAnime from "./../../assets/home_anime.gif";
+class Home extends Component {
+  render() {
+    console.log("IM IN HOME");
+    return (
+      <div className="home-container">
+        <div>
+          <div className="header-text">
+            <h1>Welcome to my portfolio!</h1>
+            <p>This is Sanikumar Sahani, a front end dev</p>
+          </div>
+
+          <div className="head-btns">
+            <Link to="/about" className="btn btn-white">
+              <p className="btn-text">Know more about me </p>
+            </Link>
+            <Link to="/contact" className="btn btn-transparent">
+              <p className="btn-text">Connect with me </p>
+            </Link>
+          </div>
+
+          <div className="splash-image">
+            <img src={homeAnime} alt="animation" className="home-anime" />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
