0cfca2823d9c8f064ba9a899e2d5c6c476b2b10d
sani2608 <sanikumarsahani@gmail.com>
2022-04-30 01:05:43 +0530

    footer component

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -2,9 +2,7 @@ import React from 'react'
 import './Projects.css';
 
 const Projects = () => {
-  return (
-    <div className='section-container'>Projects</div>
-  )
+  return <div className="section-container">I am in Projects page!</div>;
 }
 
 export default Projects
