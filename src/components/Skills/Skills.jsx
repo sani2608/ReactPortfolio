bbaff54ee3a74106c2461ca520ac36c91374f796
sani2608 <sanikumarsahani@gmail.com>
2022-05-01 03:34:13 +0530

    skills component

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,30 @@
-import React from 'react'
-import './Skills.css';
+import React from "react";
+import "./Skills.css";
+import Header from "../Header/Header";
+import FooterLink from "../FooterLink/FooterLink";
+import skillsVector from "./../../assets/skills_vector.png";
+import { skillList } from "../../assets/skillsData";
+import SkillCard from "./SkillCard";
 
+const HEADING = "My Skills.";
+const DETAILS = "Passionate about new technologies. In love with JavaScript.";
 const Skills = () => {
   return (
-    <div className='section-container'>Skills</div>
-  )
-}
+    <div className="section-container">
+      <Header heading={HEADING} details={DETAILS} />
 
-export default Skills
+      <div className="skills-card-container">
+        {skillList.map((skill, index) => {
+          return <SkillCard key={index} skillName={skill.skillName} skillUrl={skill.skillUrl} />;
+        })}
+      </div>
+
+      <FooterLink phrase="Get in" link="touch." toAddress="/contact" />
+      <div className="skills-vector-frame">
+        <img className="skills-vector" src={skillsVector} alt="skills vector" />
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
