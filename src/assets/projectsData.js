674c686b9c25fd028ea43930fd3a6e1d81c28053
sani2608 <sanikumarsahani@gmail.com>
2022-11-22 17:07:31 +0530

    added project data

--- src/assets/projectsData.js
+++ src/assets/projectsData.js
@@ -84,5 +84,68 @@ export const projectsData = [
       'https://raw.githubusercontent.com/sani2608/ReactPortfolio/main/src/assets/React-Material-App.png',
     codeUrl: 'https://github.com/sani2608/react-material-ui-social-media',
     projectUrl: 'https://sani2608.github.io/react-material-ui-social-media/'
+  },
+  {
+    projectName: 'React Formik Library Sample',
+    projectDescription:
+      "Formik is a small group of React components and hooks for building forms in React and React Native",
+    imageUrl:
+      'https://raw.githubusercontent.com/sani2608/ReactPortfolio/main/src/assets/proj6-formik.jpg',
+    codeUrl: 'https://github.com/sani2608/react-formik',
+    projectUrl: 'https://sani-formik-sample-app.netlify.app/'
+  },
+  {
+    projectName: 'SAPUI5 Todo App',
+    projectDescription:
+      "SAPUI5 is a JavaScript based framework that is used to design multi-platform business applications.",
+    imageUrl:
+      'https://raw.githubusercontent.com/sani2608/ReactPortfolio/main/src/assets/proj7-sapui5-todo.jpg',
+    codeUrl: 'https://github.com/sani2608/SAPUI5-TODO-APP',
+    projectUrl: 'https://sani-sapui5-todo-app.netlify.app/'
+  },
+  {
+    projectName: 'React Ecommerce App',
+    projectDescription:
+      "Ecommerce App built with react and contextapi.",
+    imageUrl:
+      'https://raw.githubusercontent.com/sani2608/ReactPortfolio/main/src/assets/proj8-ecommerce.jpg',
+    codeUrl: 'https://github.com/sani2608/react-ecommerce-app',
+    projectUrl: 'https://sani-ecommerce-react-app.netlify.app/'
+  },
+  {
+    projectName: 'React Redux Toolkit Sample App',
+    projectDescription:
+      "The official, opinionated, batteries-included toolset for efficient Redux development",
+    imageUrl:
+      'https://raw.githubusercontent.com/sani2608/ReactPortfolio/main/src/assets/proj9-rtk.jpg',
+    codeUrl: 'https://github.com/sani2608/react-rtk',
+    projectUrl: 'https://sani-react-redux-toolkit-sample.netlify.app/'
+  },
+  {
+    projectName: 'React Higher Order Components Sample',
+    projectDescription:
+      "A higher-order component (HOC) is an advanced technique in React for reusing component logic. HOCs are not part of the React API, per se. They are a pattern that emerges from React’s compositional nature.",
+    imageUrl:
+      'https://raw.githubusercontent.com/sani2608/ReactPortfolio/main/src/assets/proj10-hoc.jpg',
+    codeUrl: 'https://github.com/sani2608/react-higher-order-components',
+    projectUrl: 'https://sani2608.github.io/react-higher-order-components/'
+  },
+  {
+    projectName: 'React Employee Management System',
+    projectDescription:
+      "React EMS wher in we can perform CRUD operations on Employee data.",
+    imageUrl:
+      'https://raw.githubusercontent.com/sani2608/ReactPortfolio/main/src/assets/proj11-ems.jpg',
+    codeUrl: 'https://github.com/sani2608/employee-management-system',
+    projectUrl: 'https://sani2608.github.io/employee-management-system/'
+  },
+  {
+    projectName: 'Debounce and throttle sample',
+    projectDescription:
+      "Throttle: will delay executing a function. It will reduce the notifications of an event that fires multiple times. Debounce: will bunch a series of sequential calls to a function into a single call to that function. It ensures that one notification is made for an event that fires multiple times.",
+    imageUrl:
+      'https://raw.githubusercontent.com/sani2608/ReactPortfolio/main/src/assets/proj12-dbsthrtle.jpg',
+    codeUrl: 'https://github.com/sani2608/js-debounce-throttle',
+    projectUrl: 'https://sani2608.github.io/js-debounce-throttle/'
   }
 ];
